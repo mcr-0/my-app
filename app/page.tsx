a56a6eb9b12de1ae5b0ab92139d5e2809d019ec2
Michal <m@michals-mbp.home>
2024-05-17 22:12:20 +0200

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <center><h1 style={{marginTop: 400 + 'px', fontSize: 80}}>Kocham Cię Ewe</h1></center>
+    <center>
+      <h1 style={{ marginTop: 400 + "px", fontSize: 80 }}>Kocham Cię</h1>
+    </center>
   );
 }
